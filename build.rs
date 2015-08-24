86dd8fbd3ac022b9f4a1fdf31deaf6c116b211e1
Marvin Löbel <loebel.marvin@gmail.com>
2015-08-24 02:59:59 +0200

    Made build script use rustc_version crate

--- build.rs
+++ build.rs
@@ -1,20 +1,7 @@
-use std::process::Command;
+extern crate rustc_version;
 
 fn main() {
-    let out = Command::new("rustc")
-        .arg("--version")
-        .output()
-        .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
-
-    let out = String::from_utf8(out.stdout).unwrap();
-
-    let chars = &['.', '-', ' '][..];
-    let mut split = out[6..].split(chars);
-    let _major = split.next().unwrap().parse::<u32>().unwrap();
-    let minor = split.next().unwrap().parse::<u32>().unwrap();
-    let _patch = split.next().unwrap().parse::<u32>().unwrap();
-
-    if minor >= 4 {
+    if rustc_version::version_matches(">= 1.4.0") {
         println!("cargo:rustc-cfg=compiler_has_scoped_bugfix");
     }
 }
