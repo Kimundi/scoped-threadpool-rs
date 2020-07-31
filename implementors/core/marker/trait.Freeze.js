5c3a1493041965b3fcc26a0b9aafe870dfb333cc
Travis CI User <travis@example.org>
2020-07-31 23:02:27 +0000

    Documentation for Kimundi/scoped-threadpool-rs@243eba6

---
+++ implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["scoped_threadpool"] = [{"text":"impl !Freeze for <a class=\"struct\" href=\"scoped_threadpool/struct.Pool.html\" title=\"struct scoped_threadpool::Pool\">Pool</a>","synthetic":true,"types":["scoped_threadpool::Pool"]},{"text":"impl&lt;'pool, 'scope&gt; Freeze for <a class=\"struct\" href=\"scoped_threadpool/struct.Scope.html\" title=\"struct scoped_threadpool::Scope\">Scope</a>&lt;'pool, 'scope&gt;","synthetic":true,"types":["scoped_threadpool::Scope"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["scoped_threadpool"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
